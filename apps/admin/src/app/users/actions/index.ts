58725c279659f92732ce4e9b7a27d51a422904bb
Álvaro Palma <contact.alvaro.palma@gmail.com>
2024-03-10 13:30:35 -0300

    feat: integrate create user endpoint

--- apps/admin/src/app/users/actions/index.ts
+++ apps/admin/src/app/users/actions/index.ts
@@ -1,11 +1,18 @@
 import { handleError } from "@/app/core/helpers/handle-error"
+import axios from "axios"
 import { UserFormType } from "../new/page"
 
 export const USERS_PATH = "users"
 
 export const createUser = async (user: UserFormType) => {
+  const options = {
+    method: "POST",
+    url: `${process.env.NEXT_PUBLIC_API_URL}/admin/user`,
+    data: user
+  }
   try {
-    return user
+    const { data } = await axios.request(options)
+    return data
   } catch (error) {
     throw handleError(error)
   }
