57b93c385ab38b7ad93c3b43388dfa138a538458
Álvaro Palma <contact.alvaro.palma@gmail.com>
2024-03-10 13:36:38 -0300

    fix: change ^2 for Math.pow() method in distance calculator

--- apps/backend/app/features/admin/user-management/helpers/distance-calculator.ts
+++ apps/backend/app/features/admin/user-management/helpers/distance-calculator.ts
@@ -1,4 +1,4 @@
 export const distanceCalculator = (x1: number, y1: number, x2: number, y2: number): number => {
-  const distance = Math.sqrt(((x2 - x1) ^ 2) + ((y2 - y1) ^ 2))
+  const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2))
   return distance
 }
--- apps/backend/app/features/admin/user-management/helpers/get-route.ts
+++ apps/backend/app/features/admin/user-management/helpers/get-route.ts
@@ -6,6 +6,7 @@ export const getRoute = (origin: { x: number; y: number }, userList: User[]): Us
     index,
     distance: distanceCalculator(origin.x, origin.y, user.coordX, user.coordY),
   }))
+
   const sortedDistanceList = distanceList.sort((a, b) => a.distance - b.distance)
   const userListSortedByDistance = sortedDistanceList.map((dist) => userList[dist.index])
 
