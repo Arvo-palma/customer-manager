0547c46530fac257fbc7d7c4b01c6aa7f65b8787
Álvaro Palma <contact.alvaro.palma@gmail.com>
2024-03-10 19:59:12 -0300

    chore: remove old  template files

--- apps/admin/src/app/layout.tsx
+++ apps/admin/src/app/layout.tsx
@@ -9,8 +9,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Admin"
-  // description: ""
+  title: "Customer manager"
 }
 
 export default function RootLayout({
