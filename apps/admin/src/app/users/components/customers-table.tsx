58e5afd23db9f4d74d4a7f418355cf24e526f6e7
Álvaro Palma <contact.alvaro.palma@gmail.com>
2024-03-11 07:45:43 -0300

    refactor: increase table size

--- apps/admin/src/app/users/components/customers-table.tsx
+++ apps/admin/src/app/users/components/customers-table.tsx
@@ -60,10 +60,10 @@ export default function CustomersTable() {
         columns={columns}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 }
+            paginationModel: { page: 0, pageSize: 10 }
           }
         }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[5, 10, 20, 50, 100]}
         checkboxSelection
         onRowSelectionModelChange={(ids) => {
           const selectedIDs = ids.map((id) => id.toString())
